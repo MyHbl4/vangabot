77e03691b4ed29c2f7771c2206d83950aad1c369
Aleksandr Petushenko <moonbl4@mail.ru>
2022-09-10 02:34:04 +0300

    vangabot init()

---
+++ src/main/java/com/moon/vangabot/VangabotApplication.java
@@ -0,0 +1,13 @@
+package com.moon.vangabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VangabotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VangabotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/moon/vangabot/config/SpringConfig.java
@@ -0,0 +1,30 @@
+package com.moon.vangabot.config;
+
+import com.moon.vangabot.TelegramBot;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+
+@Configuration
+@AllArgsConstructor
+public class SpringConfig {
+
+    private final TelegramConfig telegramConfig;
+
+    @Bean
+    public SetWebhook setWebhookInstance() {
+        return SetWebhook.builder().url(telegramConfig.getWebhookPath()).build();
+    }
+
+    @Bean
+    public TelegramBot springWebhookBot(SetWebhook setWebhook) {
+        TelegramBot bot = new TelegramBot(setWebhook);
+
+        bot.setBotPath(telegramConfig.getWebhookPath());
+        bot.setBotUsername(telegramConfig.getBotName());
+        bot.setBotToken(telegramConfig.getBotToken());
+
+        return bot;
+    }
+}
